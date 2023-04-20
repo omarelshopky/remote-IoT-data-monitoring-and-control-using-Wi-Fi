2f4698d913266bc303540925ad387fde4544ce03
Omar Hesham <omar.elshopky202@gmail.com>
2023-04-20 04:35:37 +0200

    Initial commit

---
+++ Remote IoT data monitoring and control using Wi-Fi/main.c
@@ -0,0 +1,18 @@
+/*
+ * Remote IoT data monitoring and control using Wi-Fi.c
+ *
+ * Created: 4/20/2023 4:32:39 AM
+ * Author : Omar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
